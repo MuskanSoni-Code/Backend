b45a4f86034ba7896dfef4f1e06fcb4be7c70365
“Muskan <muskansoni.code@gmail.com>
2026-03-23 16:57:44 +0530

    route route add

--- index.mjs
+++ index.mjs
@@ -1,6 +1,16 @@
 import express from "express";
 const srever = express();
 const port = 3000;
+// srever.get=("/"(req, res)=>{
+// res.json({
+// name:"hello"
+// })
+
+srever.get("/", (req, res) => {
+  res.json({
+    name: "Hello",
+  });
+});
 srever.listen(port, () => {
   console.log(`"Server Started on "${port}`);
 });
